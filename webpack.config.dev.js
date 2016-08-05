565f33457307ed82f0d41507e18810eb682cdc6d
Cory House <housecor@gmail.com>
2016-08-04 22:05:57 -0500

    Dump unnecessary polyfill

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -6,7 +6,6 @@ export default {
   devtool: 'cheap-module-eval-source-map',
   noInfo: false,
   entry: [
-    'eventsource-polyfill', // necessary for hot reloading with IE
     'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
     './src/index'
   ],
