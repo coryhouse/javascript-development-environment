bfa23b7e5c2de2e30c518a5a8267640e76601d6d
Cory House <housecor@gmail.com>
2016-08-04 22:10:20 -0500

    Add fuelsavingsform logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log('Hello world!');
+import {calculateSavings} from './utils/fuelSavingsCalculator';
+
+function getById(id) {
+  return global.document.getElementById(id);
+}
+
+const newMpg = getById('new-mpg');
+const tradeMpg = getById('trade-mpg');
+const ppg = getById('ppg');
+const milesDriven = getById('miles-driven');
+
+const settings = {
+  newMpg: newMpg.value,
+  tradeMpg: tradeMpg.value,
+  ppg: ppg.value,
+  milesDriven: milesDriven.value
+};
+
+newMpg.onchange = (e) => {
+  settings.newMpg = e.srcElement.value;
+  calculateSavings(settings);
+};
+
+tradeMpg.onchange = (e) => {
+  settings.tradeMpg = e.srcElement.value;
+  calculateSavings(settings);
+};
+
+ppg.onchange = (e) => {
+  settings.ppg = e.srcElement.value;
+  calculateSavings(settings);
+};
+
+milesDriven.onchange = (e) => {
+  settings.milesDriven = e.srcElement.value;
+  calculateSavings(settings);
+};
+
+// let inputs = document.getElementsByTagName('input');
+// inputs.map(function(input) {
+//   input.addEventListener("change", function() {
+//     alert('hi');
+//   });
+// });
+
