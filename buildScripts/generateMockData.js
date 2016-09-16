089bf816f930fe7d9dbf55aec4ceb4483cd7252e
Cory House <housecor@gmail.com>
2016-09-15 19:07:40 -0500

    Simplify messaging.

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -3,6 +3,9 @@
    but you can enjoy realistic, but randomized data,
    and rapid page loads due to local, static data.
  */
+
+/* eslint-disable no-console */
+
 import jsf from 'json-schema-faker';
 import {schema} from './mockDataSchema';
 import fs from 'fs';
@@ -14,6 +17,6 @@ fs.writeFile("./src/api/db.json", json, function (err) {
   if (err) {
     return console.log(chalk.red(err));
   } else {
-    console.log(chalk.green("Mock data generated: " + json));
+    console.log(chalk.green("Mock data generated."));
   }
 });
