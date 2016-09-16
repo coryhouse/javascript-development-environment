db1bb7fbe117425538e9482a7869272e80e7b939
Cory House <housecor@gmail.com>
2016-09-15 22:09:21 -0500

    Support deleting in api

--- src/api/api.js
+++ src/api/api.js
@@ -6,8 +6,21 @@ export function getCalculations() {
   return get('calculations');
 }
 
+export function deleteCalculation(id) {
+  return del(`calculations/${id}`);
+}
+
 function get(url) {
   return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+// Can't call func delete since reserved word.
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+
+  return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response) {
@@ -15,5 +28,5 @@ function onSuccess(response) {
 }
 
 function onError(error) {
-  console.log(error);
+  console.log(error); // eslint-disable-line no-console
 }
