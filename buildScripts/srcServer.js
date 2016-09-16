64596672e3494028aafd5aa9fc6e22b1fd9180c1
Cory House <housecor@gmail.com>
2016-09-15 22:04:40 -0500

    Update example data in "real" API

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,8 +18,18 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.use(require('webpack-hot-middleware')(compiler));
 
 // Useful if doing client-side routing.
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join( __dirname, '../src/index.html'));
+});
+
+app.get('/calculations', function(req, res) {
+  // Hard coding for simplicity.
+  // Pretend this hits a real database
+  res.json([
+    {"id": 1,"newMpg":24,"tradeMpg":18,"pricePerGallon":"$1.34","milesDrivenPerMonth":954},
+    {"id": 2,"newMpg":28,"tradeMpg":39,"pricePerGallon":"$2.96","milesDrivenPerMonth":298},
+    {"id": 3,"newMpg":44,"tradeMpg":31,"pricePerGallon":"$3.72","milesDrivenPerMonth":264}
+  ]);
 });
 
 app.listen(port, function(err) {
