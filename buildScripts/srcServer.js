6c0a9d2c5fe9b84d9682b43fcaadadde7e00ec5f
Cory House <housecor@gmail.com>
2016-10-03 09:39:46 -0500

    Comment reformat

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -21,8 +21,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/calculations', function(req, res) {
-  // Hard coding for simplicity.
-  // Pretend this hits a real database
+  // Hard coding for simplicity. Pretend this hits a real database
   res.json([
     {"id": 1,"newMpg":24,"tradeMpg":18,"pricePerGallon":"$1.34","milesDrivenPerMonth":954},
     {"id": 2,"newMpg":28,"tradeMpg":39,"pricePerGallon":"$2.96","milesDrivenPerMonth":298},
