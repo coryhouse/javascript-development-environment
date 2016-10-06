8f3751e741eb71fc088e890942a02f40c6d376a0
Cory House <housecor@gmail.com>
2016-10-06 13:16:09 -0500

    Working jsdom example

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,27 @@
 import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
 
 describe('Our first test', () => {
   it('should pass', () => {
     expect(true).to.equal(true);
+  });
+});
+
+it('has document', function () {
+  jsdom();
+  var div = document.createElement('div')
+  expect(div.nodeName).eql('DIV')
+});
+
+describe('jsdom test', () => {
+  it('should pass', (done) => {
+    const index = fs.readFileSync('./src/index.html', "utf-8");
+    jsdom.env(index, function (err, window) {
+      const h2 = window.document.getElementById('main-heading');
+      expect(h2.innerHTML).to.equal("Fuel Savings Calculator");
+      done();
+      // free memory associated with the window
+    });
   });
 });
