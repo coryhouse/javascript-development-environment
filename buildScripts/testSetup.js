9cc0d7e7408171176b507c5cc57fdece460596a3
Cory House <housecor@gmail.com>
2016-10-06 16:57:45 -0500

    Remove unused call

--- buildScripts/testSetup.js
+++ buildScripts/testSetup.js
@@ -1,9 +1,5 @@
 // This file isn't transpiled, so must use CommonJS and ES5
 
-// Enable JSDOM to simulate browser environment in Node
-var jsdom = require('mocha-jsdom');
-jsdom();
-
 // Register babel to transpile before our tests run.
 require('babel-register')();
 
