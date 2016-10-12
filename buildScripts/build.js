7b8933416b642b96dfc0ee9c032f633687ce7ddb
Cory House <housecor@gmail.com>
2016-10-12 17:12:29 -0500

    Brevity

--- buildScripts/build.js
+++ buildScripts/build.js
@@ -5,9 +5,9 @@ import webpack from 'webpack';
 import webpackConfig from '../webpack.config.prod';
 import chalk from 'chalk';
 
-process.env.NODE_ENV = 'production'; // this assures the Babel dev config (for hot reloading) doesn't apply.
+process.env.NODE_ENV = 'production'; // this assures the Babel dev config doesn't apply.
 
-console.log(chalk.blue('Generating minified bundle for production via Webpack. This will take a moment...'));
+console.log(chalk.blue('Generating minified bundle for production. This will take a moment...'));
 
 webpack(webpackConfig).run((err, stats) => {
   if (err) { // so a fatal error occurred. Stop here.
@@ -29,7 +29,7 @@ webpack(webpackConfig).run((err, stats) => {
   console.log(`Webpack stats: ${stats}`);
 
   // if we got this far, the build succeeded.
-  console.log(chalk.green('Your app has been compiled in production mode and written to /dist. It\'s ready to roll!'));
+  console.log(chalk.green('Your app has been built for production and written to /dist!'));
 
   return 0;
 });
