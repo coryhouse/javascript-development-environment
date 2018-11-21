9f84e571fc8e909a74a19ceee6cbe4f36971ba87
Cory House <housecor@gmail.com>
2018-11-21 14:54:02 -0600

    Update deps

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -11,7 +11,10 @@ import {schema} from './mockDataSchema';
 import fs from 'fs';
 import chalk from 'chalk';
 
-const json = JSON.stringify(jsf(schema));
+// Must explicitly extend JSF with the desired faker lib.
+jsf.extend("faker", () => require("faker"));
+
+const json = JSON.stringify(jsf.generate(schema));
 
 fs.writeFile("./src/api/db.json", json, function (err) {
   if (err) {
