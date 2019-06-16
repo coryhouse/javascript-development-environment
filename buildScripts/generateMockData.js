2355faaa5371158dc190d1beabc4e8aa9de31860
Cory House <housecor@gmail.com>
2019-06-16 16:52:55 -0500

    Tweak comment

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -6,17 +6,17 @@
 
 /* eslint-disable no-console */
 
-import jsf from 'json-schema-faker';
-import {schema} from './mockDataSchema';
-import fs from 'fs';
-import chalk from 'chalk';
+import jsf from "json-schema-faker";
+import { schema } from "./mockDataSchema";
+import fs from "fs";
+import chalk from "chalk";
 
-// With latest version of json-schema-faker, must explicitly extend JSF with the fake libs you want to use.
+// Extend JSF with the fake libs you want to use.
 jsf.extend("faker", () => require("faker"));
 
 const json = JSON.stringify(jsf.generate(schema));
 
-fs.writeFile("./src/api/db.json", json, function (err) {
+fs.writeFile("./src/api/db.json", json, function(err) {
   if (err) {
     return console.log(chalk.red(err));
   } else {
