abf93585bf23c62688434e0237e7254767672fcb
Cory House <housecor@gmail.com>
2020-08-29 16:38:26 -0500

    revert

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -24,9 +24,9 @@ app.get('/', function(req, res) {
 app.get('/users', function(req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
   res.json([
-    { id: 1, firstName: 'Bob', lastName: 'Smith', email: 'bob@gmail.com' },
-    { id: 2, firstName: 'Tammy', lastName: 'Norton', email: 'tno@yahoo.com' },
-    { id: 3, firstName: 'Tina', lastName: 'Lee', email: 'lee.tina@hotmail.com' }
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
   ]);
 });
 
