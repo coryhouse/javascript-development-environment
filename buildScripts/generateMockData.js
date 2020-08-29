a07b8c4ca17ca9c54f67ca34e8e3e2cf34727b34
Cory House <housecor@gmail.com>
2020-08-29 14:21:48 -0500

    revert quotes

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -6,20 +6,20 @@
 
 /* eslint-disable no-console */
 
-import jsf from "json-schema-faker";
-import { schema } from "./mockDataSchema";
-import fs from "fs";
-import chalk from "chalk";
+import jsf from 'json-schema-faker';
+import { schema } from './mockDataSchema';
+import fs from 'fs';
+import chalk from 'chalk';
 
 // Extend JSF with the fake libs you want to use.
-jsf.extend("faker", () => require("faker"));
+jsf.extend('faker', () => require('faker'));
 
 const json = JSON.stringify(jsf.generate(schema));
 
-fs.writeFile("./src/api/db.json", json, function(err) {
+fs.writeFile('./src/api/db.json', json, function(err) {
   if (err) {
     return console.log(chalk.red(err));
   } else {
-    console.log(chalk.green("Mock data generated."));
+    console.log(chalk.green('Mock data generated.'));
   }
 });
