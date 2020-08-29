9c40f8c0e02ff312c3372bf6492b5bcbb17a6d8f
Cory House <housecor@gmail.com>
2020-08-29 16:39:31 -0500

    revert

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -10,12 +10,10 @@ const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
-app.use(
-  require('webpack-dev-middleware')(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath
-  })
-);
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
