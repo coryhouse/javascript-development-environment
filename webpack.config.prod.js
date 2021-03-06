c77605e8761697d3f90cbeecd84dbbee27333ccc
Cory House <housecor@gmail.com>
2021-03-06 08:39:42 -0600

    Reformat and fix hash

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,39 +1,39 @@
-import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export default {
-  mode: 'production',
-  devtool: 'source-map',
+  mode: "production",
+  devtool: "source-map",
   entry: {
-    main: path.resolve(__dirname, 'src/index'),
-    vendor: path.resolve(__dirname, 'src/vendor')
-  }, 
+    main: path.resolve(__dirname, "src/index"),
+    vendor: path.resolve(__dirname, "src/vendor"),
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    filename: '[name].[hash].js'
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "[name].[chunkhash].js",
   },
   plugins: [
-    // Generate an external css file with a hash in the filename
-    new MiniCssExtractPlugin({
-      filename: '[name].[hash].css',
-    }),
-
     // Create HTML file that includes reference to bundled JS.
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
+      template: "src/index.html",
       // Properties you define here are available in index.html
       // using htmlWebpackPlugin.options.varName
-      trackJSToken: 'INSERT YOUR TOKEN HERE'
+      trackJSToken: "INSERT YOUR TOKEN HERE",
+    }),
+
+    // Generate an external css file with a hash in the filename
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash].css",
     }),
   ],
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] },
+      { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },
