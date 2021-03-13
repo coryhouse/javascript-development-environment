6cbdd086781c3d3b3a15377c6133d1295cf6207b
Cory House <housecor@gmail.com>
2021-03-13 11:53:01 -0600

    add semi

--- src/index.test.js
+++ src/index.test.js
@@ -16,5 +16,5 @@ describe('index.html', () => {
     const h1 = dom.window.document.getElementsByTagName("h1")[0];
     expect(h1.innerHTML).to.equal("Users");
     dom.window.close();
-  })
-})
+  });
+});
