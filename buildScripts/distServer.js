5e6d12f2a4ce946cc4724cd8d60c37c6b21df58c
Cory House <housecor@gmail.com>
2021-03-13 12:08:01 -0600

    leave in

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -15,6 +15,25 @@ app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
+app.get("/users", function (req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    { id: 1, firstName: "Bob", lastName: "Smith", email: "bob@gmail.com" },
+    {
+      id: 2,
+      firstName: "Tammy",
+      lastName: "Norton",
+      email: "tnorton@yahoo.com",
+    },
+    {
+      id: 3,
+      firstName: "Tina",
+      lastName: "Lee",
+      email: "lee.tina@hotmail.com",
+    },
+  ]);
+});
+
 app.listen(port, function (err) {
   if (err) {
     console.log(err);
